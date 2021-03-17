d44fcca8ebec86d5194633959fd9fd037b3f38a7
Juber <zubairnuc@gmail.com>
2021-03-18 00:31:06 +0530

    initial commit

--- src/main/java/in/zubairshaikh/JavaTestApplication.java
+++ src/main/java/in/zubairshaikh/JavaTestApplication.java
@@ -2,9 +2,10 @@ package in.zubairshaikh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JavaTestApplication {
+public class JavaTestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaTestApplication.class, args);
